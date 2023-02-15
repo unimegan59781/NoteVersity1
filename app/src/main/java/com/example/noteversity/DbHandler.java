dcb361f62aa7c3129f860e49ab4eef92fe638763
unimegan59781 <up2061391@myport.ac.uk>
2023-02-15 14:08:45 +0000

    inital db handler commit

--- app/src/main/java/com/example/noteversity/DbHandler.java
+++ app/src/main/java/com/example/noteversity/DbHandler.java
@@ -1,4 +1,31 @@
 package com.example.noteversity;
 
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 public class DbHandler {
+
+        private DbModels dbModels;
+
+        private Context context;
+
+        private SQLiteDatabase db;
+
+        public DbHandler(Context c) {
+                context = c;
+        }
+
+        public DbHandler open() throws SQLException {
+                dbModels = new DbModels(context);
+                db = dbModels.getWritableDatabase();
+                return this;
+        }
+
+        public void close() {
+                dbModels.close();
+        }
+
+
 }
