2ee0fc33e5da5f0cb7de062b36c236b9120abe92
up2040449 <up2040449@myport.ac.uk>
2023-02-24 16:33:02 +0000

    notification_gui_henry

---
+++ app/src/main/java/com/example/noteversity/Notifications.java
@@ -0,0 +1,15 @@
+package com.example.noteversity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class Notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notifications);
+    }
+}
