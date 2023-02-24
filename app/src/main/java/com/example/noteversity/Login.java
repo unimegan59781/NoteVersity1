f67b196799cf22c4911ac51d0b9ddbb3adeb82bf
joshgotbags <nwankpadozie@gmail.com>
2023-02-24 15:41:23 +0000

    Sign/Login Done

---
+++ app/src/main/java/com/example/noteversity/Login.java
@@ -0,0 +1,23 @@
+package com.example.noteversity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+
+    }
+}
