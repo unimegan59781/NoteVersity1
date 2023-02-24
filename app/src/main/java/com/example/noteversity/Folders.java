5ab9955846cc6ab2c1d0929963d3bf1556b8f953
Joshua Hickey <joshuah775@gmail.com>
2023-02-24 16:00:36 +0000

    first folder+nav bar commit

---
+++ app/src/main/java/com/example/noteversity/Folders.java
@@ -0,0 +1,47 @@
+package com.example.noteversity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.GridLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Folders extends AppCompatActivity {
+
+    public android.content.Context cntx(){
+        return getApplicationContext();
+    }
+    public static int DPtoPixels(android.content.Context context, int dp) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        int pixels = (int) (dp * scale + 0.5f);
+        return pixels;
+    }
+
+    public void addFolder(android.view.View view){
+        GridLayout grid = (GridLayout) findViewById(R.id.grid);
+        AppCompatButton newFolder = new AppCompatButton(this);
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams(GridLayout.spec(
+                GridLayout.UNDEFINED,GridLayout.FILL,1f),
+                GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
+
+
+        params.setMargins(DPtoPixels(cntx(),3),DPtoPixels(cntx(),3),DPtoPixels(cntx(),3),DPtoPixels(cntx(),6));
+        newFolder.setLayoutParams(params);
+        newFolder.setBackgroundResource(R.drawable.folder_view);
+        newFolder.setText("New Folder");
+        newFolder.setGravity(Gravity.START);
+        newFolder.setGravity(Gravity.CENTER_VERTICAL);
+        newFolder.setPadding(DPtoPixels(getApplicationContext(),15),0,0,0);
+        grid.addView(newFolder);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.folders);
+        BottomNavigationView navBar = (BottomNavigationView) findViewById(R.id.bottomBar);
+        navBar.setSelectedItemId(R.id.homeButton);
+    }
+}
