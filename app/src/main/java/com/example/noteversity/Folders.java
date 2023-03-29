6f468335c22b8f6bad0a586ec76c0742017fd326
Joshua Hickey <joshuah775@gmail.com>
2023-03-29 14:02:38 +0100

    created a nav bar controller function. Needs to be included in every activity accessed by the nav bar

--- app/src/main/java/com/example/noteversity/Folders.java
+++ app/src/main/java/com/example/noteversity/Folders.java
@@ -1,18 +1,24 @@
 package com.example.noteversity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.GridLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Folders extends AppCompatActivity {
 
-    public android.content.Context cntx(){
+    public android.content.Context cntx() {
         return getApplicationContext();
     }
+
     public static int DPtoPixels(android.content.Context context, int dp) {
         float scale = context.getResources().getDisplayMetrics().density;
         int pixels = (int) (dp * scale + 0.5f);
@@ -20,37 +26,54 @@ public class Folders extends AppCompatActivity {
     }
 
 
-//    public String folderTitle(){
+    //    public String folderTitle(){
 //
 //    }
-    public void addFolder(android.view.View view){
+    public void addFolder(android.view.View view) {
         GridLayout grid = (GridLayout) findViewById(R.id.grid);
         AppCompatButton newFolder = new AppCompatButton(this);
         GridLayout.LayoutParams params = new GridLayout.LayoutParams(GridLayout.spec(
-                GridLayout.UNDEFINED,GridLayout.FILL,1f),
-                GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
+                GridLayout.UNDEFINED, GridLayout.FILL, 1f),
+                GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f));
 
 
-        params.setMargins(DPtoPixels(cntx(),3),DPtoPixels(cntx(),3),DPtoPixels(cntx(),3),DPtoPixels(cntx(),6));
+        params.setMargins(DPtoPixels(cntx(), 3), DPtoPixels(cntx(), 3), DPtoPixels(cntx(), 3), DPtoPixels(cntx(), 6));
         newFolder.setLayoutParams(params);
         newFolder.setBackgroundResource(R.drawable.folder_view);
         newFolder.setText("New Folder");
         newFolder.setGravity(Gravity.START);
         newFolder.setGravity(Gravity.CENTER_VERTICAL);
-        newFolder.setPadding(DPtoPixels(getApplicationContext(),15),0,0,0);
+        newFolder.setPadding(DPtoPixels(getApplicationContext(), 15), 0, 0, 0);
         grid.addView(newFolder);
     }
 
+    public void navBarController(){
+        BottomNavigationView bottomBar = findViewById(R.id.bottomBar);
+        bottomBar.setSelectedItemId(R.id.homeButton);
+        bottomBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.notifButton) {
+                    Intent intent = new Intent(Folders.this, Notifications.class);
+                    startActivity(intent);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.folders);
-        BottomNavigationView navBar = findViewById(R.id.bottomBar);
-        navBar.setSelectedItemId(R.id.homeButton);
-        //add a functino to query folders from database
+        navBarController();
+
     }
 
-//  Functions that validates the folder title fits within 0 - 32 characters
+    //  Functions that validates the folder title fits within 0 - 32 characters
     public static String checkFolderTitle(String[] title) {
         if (title.length == 0) {
             return "Please enter a title";
@@ -61,5 +84,3 @@ public class Folders extends AppCompatActivity {
         }
     }
 }
-
-
--- app/src/main/java/com/example/noteversity/Notifications.java
+++ app/src/main/java/com/example/noteversity/Notifications.java
@@ -1,15 +1,54 @@
 package com.example.noteversity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class Notifications extends AppCompatActivity {
+
+
+    public void navBarController(){
+        BottomNavigationView bottomBar = findViewById(R.id.bottomBar);
+        bottomBar.setSelectedItemId(R.id.notifButton);
+        bottomBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.homeButton){
+                    Intent intent = new Intent(Notifications.this, Folders.class);
+                    startActivity(intent);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications);
+        navBarController();
+
     }
+
+
+
+
+//
+
 }
+
+
+
