941cd6629435520f7d1d3702204b634ad64dbb87
unimegan59781 <up2061391@myport.ac.uk>
2023-03-31 14:01:32 +0100

    megan notes page layout and gestures/buttons functioning to do db link now

--- app/src/main/java/com/example/noteversity/NotesPages.java
+++ app/src/main/java/com/example/noteversity/NotesPages.java
@@ -2,8 +2,12 @@ package com.example.noteversity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -14,7 +18,19 @@ import androidx.appcompat.widget.AppCompatButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NotesPages extends AppCompatActivity {
+
+    public android.content.Context cntx(){
+        return getApplicationContext();
+    }
+    public static int DPtoPixels(android.content.Context context, int dp) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        int pixels = (int) (dp * scale + 0.5f);
+        return pixels;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +39,22 @@ public class NotesPages extends AppCompatActivity {
         FloatingActionButton createNoteButton = findViewById(R.id.newNoteBtn);
         ImageButton backButton = findViewById(R.id.backButton);
 
+        GridLayout grid = (GridLayout) findViewById(R.id.grid);
+        List<String> allNotes = Arrays.asList("dog", "cat");
+        if (allNotes != null){
+            for (String name : allNotes) {
+                AppCompatButton newNote = createNote(name);
+                grid.addView(newNote);
+                noteInteractions(newNote, grid);
+            }
+        }
+
         createNoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(NotesPages.this, NoteCreation.class));
             }
         });
-
-
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,53 +64,91 @@ public class NotesPages extends AppCompatActivity {
         });
 
     };
+
+    public void noteInteractions(AppCompatButton note, GridLayout grid) {
+        View noteView = getNote(note.getText().toString(), grid); // uses get function to find view of the folder from grid layout
+        String noteName = note.getText().toString();
+        noteView.setOnTouchListener(new View.OnTouchListener() {
+            private final GestureDetector gestDetector = new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > 100 && Math.abs(velocityX) > 100) {
+                            if (diffX < 0) {
+                                onSwipeLeft();
+                            }
+                            return super.onFling(e1, e2, velocityX, velocityY);
+                        }
+                    }
+                    return super.onFling(e1, e2, velocityX, velocityY);
+                }
+
+                public void onSwipeLeft() {
+                    noteView.setBackgroundColor(Color.RED);
+                }
+
+                @Override // double tap to edit name
+                public boolean onDoubleTap(MotionEvent e) {
+                    noteView.setBackgroundColor(Color.YELLOW);
+                    //Log.d("Folder", folderID); // logcat test prove valid folderID
+                    // TO CHNAGE FOLDER NAME
+                    return super.onDoubleTap(e); // passes event as super to overide touch
+                }
+
+                @Override
+                public boolean onSingleTapConfirmed(MotionEvent e) {
+                    noteView.setBackgroundColor(Color.BLUE);
+                    // TO GO TO VIEW
+                    return super.onSingleTapConfirmed(e);
+                }
+
+            });
+
+            public boolean onTouch(View v, MotionEvent event) {
+                gestDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+
+    }
+
+    public View getNote(String noteName, GridLayout grid) { // function that loops the views in folder grid layout to find location of view with desired name
+        for(int i = 0; i < grid.getChildCount(); i++) {
+            View noteView = grid.getChildAt(i);
+            if(noteView instanceof Button) { // itterates through folders in grid
+                Button gridNote = (Button) noteView; // casts to appcompatbutton (button) to see if folder exisits
+                if(gridNote.getText().toString().equals(noteName)) { // use name as unquie identifyer of view
+                    return noteView; // returns view of folderName
+                }
+            }
+        }
+        return null; // folder doesn't exist (error checker - shouldn't occcur)
+    }
+
+    public void deleteFolder(View noteView, GridLayout grid, String name) { // delete folder from view
+        grid.removeView(noteView); // removes view in grid layout folder with view thats be named
+        //dbHandler.deleteFolder(name);
+        // TO ADD DB DELETE CODE
+        };
+
+    public AppCompatButton createNote(String noteName){
+        AppCompatButton newNote = new AppCompatButton(this);
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams(GridLayout.spec(
+                GridLayout.UNDEFINED,GridLayout.FILL,1f),
+                GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
+
+        params.setMargins(DPtoPixels(cntx(),3),DPtoPixels(cntx(),3),DPtoPixels(cntx(),3),DPtoPixels(cntx(),6));
+        newNote.setLayoutParams(params);
+        newNote.setBackgroundResource(R.drawable.folder_view); /// TO DO SET PNG
+        newNote.setText(noteName); // use will name function
+        newNote.setGravity(Gravity.START);
+        newNote.setGravity(Gravity.CENTER_VERTICAL);
+        newNote.setPadding(DPtoPixels(getApplicationContext(),15),0,0,0);
+        return newNote;
+    }
 }
-
-//
-//newFolder.setLayoutParams(params);
-//        newFolder.setBackgroundResource(R.drawable.folder_view);
-//        newFolder.setText(folderName); // use will name function
-//        newFolder.setGravity(Gravity.START);
-//        newFolder.setGravity(Gravity.CENTER_VERTICAL);
-//        newFolder.setPadding(DPtoPixels(getApplicationContext(),15),0,0,0);
-//        return newFolder;
-//
-//        }
-//
-//
-//@SuppressLint("ClickableViewAccessibility")
-//public void folderAdd(android.view.View view){
-//        GridLayout folder = (GridLayout) findViewById(R.id.grid);
-//        String folderName = nameFolder();
-//        AppCompatButton newFolder = createFolder(folderName);
-//        folder.addView(newFolder);
-//        folderInteractions(newFolder, folder);
-//        dbHandler.insertFolder(1, newFolder.getText().toString()); // inserts new folder into db folders table
-//
-//        }
-//
-//public View getFolder(String folderName, GridLayout folder) { // function that loops the views in folder grid layout to find location of view with desired name
-//        for(int i = 0; i < folder.getChildCount(); i++) {
-//        View folderLocation = folder.getChildAt(i);
-//        if(folderLocation instanceof Button) { // itterates through folders in grid
-//        Button gridFolder = (Button) folderLocation; // casts to appcompatbutton (button) to see if folder exisits
-//        if(gridFolder.getText().toString().equals(folderName)) { // use name as unquie identifyer of view
-//        return folderLocation; // returns view of folderName
-//        }
-//        }
-//        }
-//        return null; // folder doesn't exist (error checker - shouldn't occcur)
-//        }
-//
-//public void deleteFolder(View folderName, GridLayout folder, String name) { // delete folder from view
-//        folder.removeView(folderName); // removes view in grid layout folder with view thats be named
-//        dbHandler.deleteFolder(name);
-//        // TO ADD DB DELETE CODE
-//        };
-
-
-
-
 
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //
